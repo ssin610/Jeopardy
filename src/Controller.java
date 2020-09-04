b84d007195eff6348c1797ad348e81c4893afa52
root <root@DESKTOP-5D9ACJ9.localdomain>
2020-09-05 00:34:57 +1200

    Change scenes when play button is pushed

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,25 @@
 package assignment2ssin610.src;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    public void changeScreenButtonPushed(ActionEvent event) throws IOException
+    {
+        Parent viewParent = FXMLLoader.load(getClass().getResource("scene2.fxml"));
+        Scene viewScene = new Scene(viewParent);
+        
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(viewScene);
+        window.show();
+    }
 }
